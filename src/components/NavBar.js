47f788c2453bc488720831d0e402620f00d5890b
Exequiel-Carrizo <exzer11@gmail.com>
2022-08-24 20:39:17 -0300

    Primer commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import logo from '../xizt.svg'
+
+const NavBar = () => {
+    return (
+        <div className="Navbar-Contenedor">
+            <img src={logo} className="App-logo-nav" alt="logo" />
+            <div className="Links">
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Sección 1</a></li>
+                    <li><a href="#">Sección 2</a></li>
+                    <li><a href="#">Sección 3</a></li>
+                </ul>
+            </div>
+            <div className="Caja-Boton">
+                <button className="Boton">Ingresa</button>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
