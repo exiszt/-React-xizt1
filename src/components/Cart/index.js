346b3c44d4eda60ac64f23d68e793f10f42e9f51
Exequiel-Carrizo <exzer11@gmail.com>
2022-10-01 23:43:25 -0300

    12º commit: Item Collection 2 (act.1)

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,14 +1,67 @@
 import { useContext, useEffect, useState } from "react"
 import { CartContext } from "../../context/CartContext"
 import { Link } from "react-router-dom"
+import { collection, addDoc, getFirestore, updateDoc, doc } from 'firebase/firestore'
+import moment from "moment/moment"
 import './style.css'
 // import Formulario from "../Formulario"
 
 const Cart = () => {
-
+  const [order, setOrder] = useState({
+    buyer: {
+      name: 'Juan',
+      phone: '123456789',
+      email: 'test@test.com'
+    },
+    items: [],
+    total: 0,
+    date: '',
+  })
   const { cart, clearCart, deleteProduct } = useContext(CartContext)
   const [total, setTotal] = useState(0)
   const [update, setUpdate] = useState(false)
+  const db = getFirestore()
+
+  const createOrder = () => {
+    setOrder((currentOrder) => {
+      return {
+        currentOrder,
+        items: cart,
+        total: cart.reduce((acc, item) => acc + item.price * item.quantity, 0),
+        date: moment().format('DD/MM/YYYY, h:mm:ss a'),
+      }
+    })
+    // Búsqueda de la colección "orders" en database
+    const query = collection(db, 'orders')
+    addDoc(query, order)
+      .then(({ id }) => {
+        console.log(id)
+        // updateStockProducts(cart)
+        alert('Felicidades por tu compra')
+      })
+      .catch(() => alert('Tu compra no pudo completarse'))
+  }
+
+  // const updateStockProducts = () =>{
+  //   const idOrder = '4ffQh77irMfpd9teAxDS'
+  //   const order = {
+  //     buyer: {
+  //       name: 'Juan',
+  //       phone: '123456789',
+  //       email: 'test@test.com'
+  //     },
+  //     items: cart.pop(),
+  //     total: cart.reduce((valorPasado, valorActual) =>
+  //       valorPasado + valorActual.price * valorActual.quantity,
+  //       0
+  //     ),
+  //     date: moment().format()
+  //   }
+  //   const queryUpdate = doc(db, 'orders', idOrder)
+  //   updateDoc(queryUpdate, order)
+  //   .then((response)=>{console.log(response)})
+  //   .catch((error) => {console.log(error)})
+  // }
 
   useEffect(() => {
     setTotal((cart.reduce((acc, prod) => acc + prod.price * prod.quantity, 0)))
@@ -17,15 +70,15 @@ const Cart = () => {
   const deleteCartItem = (id) => {
     deleteProduct(id)
     !update
-    ? setUpdate(true)
-    : setUpdate(false)
+      ? setUpdate(true)
+      : setUpdate(false)
   }
 
   const deleteCart = () => {
     clearCart()
     !update
-    ? setUpdate(true)
-    : setUpdate(false)
+      ? setUpdate(true)
+      : setUpdate(false)
   }
 
   return (
@@ -59,11 +112,11 @@ const Cart = () => {
         <div>
           <h2 className="cartTitulo">Total: ${total}</h2>
           <button className="cartBoton eliminarCart" onClick={deleteCart}>Elminar Cart</button>
-          <Link to={'/formulario'}>
-            <button className="cartBoton confirmarCompra">
-              Confirmar compra
-            </button>
-          </Link>
+          {/* <Link to={'/formulario'}> */}
+          <button onClick={createOrder} className="cartBoton confirmarCompra">
+            Confirmar compra
+          </button>
+          {/* </Link> */}
         </div>
       }
     </div>
