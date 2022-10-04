2d49258d0d68a9255b9e68fc3cbf1b22b8ec980c
Exequiel-Carrizo <exzer11@gmail.com>
2022-10-04 20:24:37 -0300

    12º commit: Item Collection 2 (act.2)

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,67 +1,98 @@
 import { useContext, useEffect, useState } from "react"
 import { CartContext } from "../../context/CartContext"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { collection, addDoc, getFirestore, updateDoc, doc } from 'firebase/firestore'
 import moment from "moment/moment"
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
 import './style.css'
 // import Formulario from "../Formulario"
 
 const Cart = () => {
-  const [order, setOrder] = useState({
-    buyer: {
-      name: 'Juan',
-      phone: '123456789',
-      email: 'test@test.com'
-    },
-    items: [],
-    total: 0,
-    date: '',
-  })
   const { cart, clearCart, deleteProduct } = useContext(CartContext)
   const [total, setTotal] = useState(0)
   const [update, setUpdate] = useState(false)
   const db = getFirestore()
+  const navigate = useNavigate()
+  const [users, setUsers] = useState({
+    name: "",
+    phone: "",
+    email: "",
+  })
 
   const createOrder = () => {
-    setOrder((currentOrder) => {
-      return {
-        currentOrder,
-        items: cart,
-        total: cart.reduce((acc, item) => acc + item.price * item.quantity, 0),
-        date: moment().format('DD/MM/YYYY, h:mm:ss a'),
-      }
-    })
+    const order = {
+      buyer: {
+        name: `${users.name}`,
+        phone: `${users.phone}`,
+        email: `${users.email}`,
+      },
+      items: cart,
+      total: cart.reduce((oldValue, currentValue) => oldValue + (currentValue.price * currentValue.quantity), 0),
+      date: moment().format()
+    }
     // Búsqueda de la colección "orders" en database
     const query = collection(db, 'orders')
     addDoc(query, order)
       .then(({ id }) => {
-        console.log(id)
-        // updateStockProducts(cart)
-        alert('Felicidades por tu compra')
+        console.log(`ID de la compra: ${id}`)
+        updateStockProducts(cart)
+        Toastify({
+          text: (`¡Gracias por su compra, ${users.name}!`),
+          duration: 3000,
+          destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top",
+          position: "right",
+          stopOnFocus: true,
+          style: {
+            background: "deepskyblue",
+          },
+          onClick: function () { }
+        }).showToast()
       })
-      .catch(() => alert('Tu compra no pudo completarse'))
+      .catch(() => {
+        Toastify({
+          text: (`¡Lo sentimos, ${users.name}! Hubo un problema con su compra`),
+          duration: 3000,
+          destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top",
+          position: "right",
+          stopOnFocus: true,
+          style: {
+            background: "red",
+          },
+          onClick: function () { }
+        }).showToast()
+      }
+      )
   }
 
-  // const updateStockProducts = () =>{
-  //   const idOrder = '4ffQh77irMfpd9teAxDS'
-  //   const order = {
-  //     buyer: {
-  //       name: 'Juan',
-  //       phone: '123456789',
-  //       email: 'test@test.com'
-  //     },
-  //     items: cart.pop(),
-  //     total: cart.reduce((valorPasado, valorActual) =>
-  //       valorPasado + valorActual.price * valorActual.quantity,
-  //       0
-  //     ),
-  //     date: moment().format()
-  //   }
-  //   const queryUpdate = doc(db, 'orders', idOrder)
-  //   updateDoc(queryUpdate, order)
-  //   .then((response)=>{console.log(response)})
-  //   .catch((error) => {console.log(error)})
-  // }
+  const updateStockProducts = () => {
+    cart.forEach(product => {
+      const queryUpdate = doc(db, 'items', product.id);
+      updateDoc(queryUpdate, {
+        category: product.category,
+        description: product.description,
+        image: product.image,
+        price: product.price,
+        title: product.title,
+        stock: product.stock,
+      })
+        .then(() => {
+          if (cart[cart.length - 1].id === product.id) {
+            deleteCart();
+            navigate('/');
+          }
+        })
+        .catch(() => {
+          console.log('Error al actualizar stock');
+        })
+    });
+  }
 
   useEffect(() => {
     setTotal((cart.reduce((acc, prod) => acc + prod.price * prod.quantity, 0)))
@@ -81,10 +112,44 @@ const Cart = () => {
       : setUpdate(false)
   }
 
+  const confirm = (event) => {
+    event.preventDefault()
+    if (!users.name || !users.phone || !users.email) {
+      Toastify({
+        text: ('Complete todos los campos'),
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+          background: "red",
+        },
+        onClick: function () { }
+      }).showToast()
+    } else {
+      createOrder()
+      deleteCart()
+    }
+  }
+
+  const handleChange = (event) => {
+    const { target } = event;
+    const { name, value } = target;
+
+    const newValues = {
+      ...users,
+      [name]: value,
+    };
+    setUsers(newValues);
+  }
+
   return (
 
     <div>
-      <h2 className="cartTitulo">Cart</h2>
+      <h1 className="cartTitulo">Cart</h1>
 
       {cart.map((item) => (
         <section key={item.id} className="cartSubTitles" >
@@ -104,7 +169,10 @@ const Cart = () => {
         ?
         (
           <div>
-            <h2>No hay productos en tu carrito</h2>
+            <h2 className="cartTitulo">No hay productos en tu carrito</h2>
+            <Link to={'/'}>
+              <button className="cartTitulo verMas">Volver al inicio</button>
+            </Link>
           </div>
         )
 
@@ -112,11 +180,48 @@ const Cart = () => {
         <div>
           <h2 className="cartTitulo">Total: ${total}</h2>
           <button className="cartBoton eliminarCart" onClick={deleteCart}>Elminar Cart</button>
-          {/* <Link to={'/formulario'}> */}
-          <button onClick={createOrder} className="cartBoton confirmarCompra">
-            Confirmar compra
-          </button>
-          {/* </Link> */}
+          <div>
+            <h2 className="cartTitulo">Finalizar compra</h2>
+          </div>
+          <form className="contFormulario" onSubmit={confirm}>
+            <section className="formulario">
+              <div>
+                <input
+                  id="name"
+                  name="name"
+                  className="input"
+                  placeholder="Ingrese su nombre"
+                  value={users.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <input
+                  id="phone"
+                  name="phone"
+                  className="input"
+                  placeholder="Ingrese su teléfono"
+                  value={users.phone}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <input id="email"
+                  name="email"
+                  className="input"
+                  placeholder="Ingrese su e-mail"
+                  value={users.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <button className="cartBoton confirmarCompra"
+                  type="submit">
+                  Confirmar compra
+                </button>
+              </div>
+            </section>
+          </form>
         </div>
       }
     </div>
